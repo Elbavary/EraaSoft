4eff64c1db8ce70ac86760efc67f5f236043f53b
Khaled Gamel <progkhaledgamel@gmail.com>
2024-05-28 11:59:33 +0300

    Task 1 Part 1

---
+++ Task 1 Part 1/Account.cs
@@ -0,0 +1,33 @@
+﻿
+namespace EraaSoft
+{
+    internal class Account
+    {
+        public Account(string name, decimal balance)
+        {
+            _name = name;
+            _balance = balance;
+        }
+        private string _name;
+        private decimal _balance;
+        public void Deposit(decimal amount)
+        {
+            _balance += amount;
+        }
+        public void Withdraw(decimal amount)
+        {
+            _balance -= amount;
+        }
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+        }
+        public void Print()
+        {
+            Console.WriteLine($"Welcome {_name} Your balance is {_balance}");
+        }
+    }
+}
---
+++ Task 1 Part 1/Program.cs
@@ -0,0 +1,25 @@
+﻿
+namespace EraaSoft
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account = new Account("Khaled", 5000);
+            account.Print();
+            account.Deposit(2000);
+            account.Print();
+            account.Withdraw(4000);
+            account.Print();
+
+            Console.WriteLine("================================");
+
+            Account khaled = new("Khaled Gamel", 12000);
+            khaled.Print();
+            khaled.Deposit(2000);
+            khaled.Print();
+            khaled.Withdraw(4000);
+            khaled.Print();
+        }
+    }
+}
