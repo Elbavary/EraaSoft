4822c43ea8bb06162cc4c7e7d5075e88751f92b7
Khaled Gamel <progkhaledgamel@gmail.com>
2024-05-28 16:57:07 +0300

    Task 1 Part 2

---
+++ Task 1 Part 2/Account.cs
@@ -0,0 +1,43 @@
+﻿
+namespace EraaSoft
+{
+    internal class Account
+    {
+        public Account(string name, decimal balance)
+        {
+            _name = name;
+            _balance = balance;
+        }
+        private string _name;
+        private decimal _balance;
+        public bool Deposit(decimal amount)
+        {
+            if (amount > 0)
+            {
+                _balance += amount;
+                return true;
+            }
+            return false;
+        }
+        public bool Withdraw(decimal amount)
+        {
+            if (amount > 0 && amount < _balance)
+            {
+                _balance -= amount;
+                return true;
+            }
+            return false;
+        }
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+        }
+        public void Print()
+        {
+            Console.WriteLine($"Welcome {_name} Your balance is {_balance}");
+        }
+    }
+}
---
+++ Task 1 Part 2/Program.cs
@@ -0,0 +1,76 @@
+﻿
+using System.Security.Principal;
+
+namespace EraaSoft
+{
+    internal class Program
+    {
+        public enum UserOption
+        {
+            Withdraw = 1,
+            Deposit,
+            Print,
+            Quit
+        }
+        static void Main(string[] args)
+        {
+            Account account = new Account("Khaled", 5000);
+
+            UserOption selectedOption = ReadUserOption();
+
+            switch (selectedOption)
+            {
+                case UserOption.Withdraw:
+                    Console.WriteLine("You selected Withdraw");
+                    DoWithdraw(account);
+                    break;
+                case UserOption.Deposit:
+                    Console.WriteLine("You selected Deposit");
+                    DoDeposit(account);
+                    break;
+                case UserOption.Print:
+                    Console.WriteLine("You selected Print");
+                    DoPrint(account);
+                    break;
+                case UserOption.Quit:
+                    Console.WriteLine("You selected Quit");
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Invalid selection.");
+                    break;
+            }
+        }
+        private static void DoDeposit(Account account)
+        {
+            Console.WriteLine("Enter amount to deposit:");
+            decimal amount = Convert.ToDecimal(Console.ReadLine());
+            account.Deposit(amount);
+            account.Print();
+        }
+
+        private static void DoWithdraw(Account account)
+        {
+            Console.WriteLine("Enter amount to withdraw:");
+            decimal amount = Convert.ToDecimal(Console.ReadLine());
+            account.Withdraw(amount);
+            account.Print();
+        }
+        private static void DoPrint(Account account)
+        {
+            account.Print();
+        }
+        private static UserOption ReadUserOption()
+        {
+            int selection;
+            do
+            {
+                Console.WriteLine("Please select an option (1-4):");
+                selection = Convert.ToInt32(Console.ReadLine());
+            }
+            while (selection < 1 || selection > 4);
+
+            return (UserOption)selection;
+        }
+    }
+}
